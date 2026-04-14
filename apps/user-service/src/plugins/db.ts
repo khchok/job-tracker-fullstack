5b84db2d3be057287b1cca2a3657fc69b1e3282f
Chok Khar Hui <chok072056@gmail.com>
2026-04-14 23:53:14 +0800

    fix: reuse pg connection

--- apps/user-service/src/plugins/db.ts
+++ apps/user-service/src/plugins/db.ts
@@ -1,18 +1,12 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
-import { PrismaClient } from "../generated/prisma/client";
+import prisma from "../lib/prisma";
 
 const prismaPlugin = async (fastify: FastifyInstance) => {
-  const prisma = new PrismaClient({
-    adapter: new PrismaPg({
-      connectionString: process.env.USER_SERVICE_DATABASE_URL,
-    }),
-  });
   await prisma.$connect();
   fastify.decorate("prisma", prisma);
-  fastify.addHook("onClose", async (fastify: FastifyInstance) => {
-    await fastify.prisma.$disconnect();
+  fastify.addHook("onClose", async () => {
+    await prisma.$disconnect();
   });
 };
 
