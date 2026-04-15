c2d0765cd0d2b1caa0ddfd7a057104ffb1f9fff3
Chok Khar Hui <chok072056@gmail.com>
2026-04-15 12:21:46 +0800

    fix: remove cross service validation

--- apps/job-service/src/plugins/jwt.ts
+++ apps/job-service/src/plugins/jwt.ts
@@ -1,48 +1,43 @@
-import jwt from "@fastify/jwt";
-import * as Sentry from "@sentry/node";
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import fp from "fastify-plugin";
-import { JwtPayload } from "shared-types";
-import { validateSession } from "../lib/session-validator";
+import jwt from '@fastify/jwt';
+import * as Sentry from '@sentry/node';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import fp from 'fastify-plugin';
+import { JwtPayload } from 'shared-types';
 
 async function jwtPlugin(fastify: FastifyInstance) {
   await fastify.register(jwt, {
     secret: process.env.JWT_PUBLIC_KEY!,
     cookie: {
-      cookieName: "token",
-      signed: false,
-    },
+      cookieName: 'token',
+      signed: false
+    }
   });
 
-  fastify.decorate(
-    "authenticate",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      try {
-        await request.jwtVerify();
-      } catch (err) {
-        return reply.send(err);
-      }
+  fastify.decorate('authenticate', async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (err) {
+      return reply.send(err);
+    }
 
-      const payload = request.user as JwtPayload;
-      if (!payload?.jti) {
-        return reply.status(401).send({ error: "Invalid token" });
-      }
+    const payload = request.user as JwtPayload;
+    if (!payload?.jti) {
+      return reply.status(401).send({ error: 'Invalid token' });
+    }
 
-      try {
-        const valid = await validateSession(payload.jti);
-        if (!valid) {
-          return reply.status(401).send({ error: "Invalid session" });
-        }
-      } catch(err) {
-        Sentry.captureException(err);
-        return reply
-          .status(503)
-          .send({ error: "Authentication service unavailable" });
+    try {
+      const valid = true; // TODO: due to server overload, temporarily skip session validation.
+      // const valid = await validateSession(payload.jti);
+      if (!valid) {
+        return reply.status(401).send({ error: 'Invalid session' });
       }
-    },
-  );
+    } catch (err) {
+      Sentry.captureException(err);
+      return reply.status(503).send({ error: 'Authentication service unavailable' });
+    }
+  });
 }
 
 export default fp(jwtPlugin, {
-  name: "jwt",
+  name: 'jwt'
 });
